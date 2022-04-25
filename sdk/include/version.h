eacfa57be9fdbe6ad88007bda4aeae349f34bc64
Konstantin Bläsi <kblaesi@gmail.com>
2022-04-26 00:48:57 +0200

    Bump to v1.8.3.

--- sdk/include/version.h
+++ sdk/include/version.h
@@ -7,10 +7,10 @@
 
 #define LIB_YASDI_VER1 1
 #define LIB_YASDI_VER2 8
-#define LIB_YASDI_VER3 2
+#define LIB_YASDI_VER3 3
 #define LIB_YASDI_VER4 0
 
-#define LIB_YASDI_VERSION "1.8.2"
-#define LIB_YASDI_VERSION_FULL "1.8.2Build0"
+#define LIB_YASDI_VERSION "1.8.3"
+#define LIB_YASDI_VERSION_FULL "1.8.3Build0"
 
 #endif
